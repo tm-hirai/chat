ed42e1d7a074baf7463a4df32e85a21ff0cec6c1
tm-hirai <tm-hirai@kddi.com>
2020-06-10 15:23:50 +0900

    github認証を追加

---
+++ app.js
@@ -0,0 +1,84 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var helmet = require('helmet');
+var passport = require('passport');
+var GitHubStrategy = require('passport-github2').Strategy;
+var session = require('express-session');
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+var GITHUB_CLIENT_ID = "39d2ae6e44430dfa51c7";
+var GITHUB_CLIENT_SECRET = "7044c2241eb22fbbe988b1f2ef2379c934810ec4";
+passport.use(new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: "http://localhost:8000/auth/github/callback"
+},
+  function (accessToken, refreshToken, profile, done) {
+    // console.log(profile);
+    process.nextTick(function () {
+      return done(null, profile);
+    });
+  }
+));
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+app.use(helmet());
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'Wri-0s@8p7Eswu#+ip@6', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.get('/auth/github',
+  passport.authenticate('github', { scope: ['user:email'] }));
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+app.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
