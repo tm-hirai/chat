e713ae97e01bfc434d6b3b21483d95a2a835bb21
KDDI <kddi@MacBook-Pro.local>
2020-06-12 15:21:48 +0900

    メッセージの投稿

---
+++ app/entry.js
@@ -0,0 +1,32 @@
+'use strict';
+import $ from 'jquery';
+const global = Function('return this;')();
+global.jQuery = $;
+// import bootstrap from 'bootstrap';
+
+$('#post-message-button').on('click', () => {
+  const button = $('#post-message-button')
+  const userId = button.data('user-id');
+  const channelId = button.data('channel-id');
+  const content = $('#message-content').val();
+
+  console.log(userId);
+  console.log(channelId);
+  console.log(content);
+
+  // const candidateId = button.data('candidate-id');
+  // const availability = parseInt(button.data('availability'));
+  // const nextAvailability = (availability + 1) % 3;
+  $.post(`/channels/${channelId}/message`,
+    { userId: userId, content: content },
+    (data) => {
+      console.log(data);
+      // button.data('availability', data.availability);
+      // const availabilityLabels = ['欠', '？', '出'];
+      // button.text(availabilityLabels[data.availability]);
+
+      // const buttonStyles = ['btn-danger', 'btn-secondary', 'btn-success'];
+      // button.removeClass('btn-danger btn-secondary btn-success');
+      // button.addClass(buttonStyles[data.availability]);
+    });
+});
--- routes/channels.js
+++ routes/channels.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const uuid = require('uuid');
 const Channel = require('../models/channel');
 const Messages = require('../models/message');
+const Message = require('../models/message');
 
 /* GET users listing. */
 // チャンネル作成ページ
@@ -36,8 +37,21 @@ router.post('/', function (req, res, next) {
 });
 // メッセージ作成、削除
 router.post('/:channelId/message', function (req, res, next) {
-  console.log('message' + req.params.channelId);
-  res.render('channel', { title: 'Express', user: req.user });
+  console.log(req.params.channelId);
+  console.log(req.body);
+
+  const channelId = req.params.channelId;
+  const content = req.body.content;
+  const userId = req.body.userId;
+
+  Message.create({
+    content: content,
+    postedBy: userId,
+    channelId: channelId
+  }).then(() => {
+    res.json({ status: 'OK', content: content });
+  });
+  // res.render('channel', { title: 'Express', user: req.user });
 });
 
 
